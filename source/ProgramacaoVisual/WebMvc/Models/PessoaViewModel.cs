2b1d41119a6726327a97c8c73a1647e0317bd05a
silverhenrique <pablo.vargas@opet.edu.br>
2018-03-06 14:35:26 +0000

    Aula 06-03-18

---
+++ source/ProgramacaoVisual/WebMvc/Models/PessoaViewModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace WebMvc.Models
+{
+    public class PessoaViewModel
+    {
+        // CamelCase
+        // pascalCase
+        //snake_case
+
+        public IList<Pessoa> Items {get;set;}
+    }
+}
